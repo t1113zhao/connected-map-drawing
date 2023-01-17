d37ac45e5f850fedfed1a9df6f624ad220e26132
t1113zhao <zhaotian1998@gmail.com>
2023-01-17 12:25:49 -0800

    Create uids.tsx

---
+++ src/state/uids.tsx
@@ -0,0 +1,40 @@
+
+export enum StateIdType {
+    AGENCY = "AGENCY",
+    LINE = "LINE",
+    SERVICE = "SERVICE",
+    TRANSFER = "TRANSFER",
+    TRACK = "TRACK",
+    STATION = "STATION",
+    BLOCK = "BLOCK",
+    EDGE = "EDGE",
+    TRACK_SEGMENT = "SEGMENT"
+}
+
+type UidObject = {
+    type: StateIdType,
+    primaryId: string,
+    parentId: string | null
+}
+
+export const UID_SPLITTER = "-"
+
+export function getUidObjectFromUidString(uid: string): UidObject | null {
+    const array = uid.split(UID_SPLITTER)
+
+    if (array[0] in StateIdType) {
+        return {
+            type: array[0] as StateIdType,
+            primaryId: array[1],
+            parentId: array.length > 2 ? array[2] : null
+        }
+    }
+    return null
+}
+
+export function getUidStringFromUidObject(uidObject: UidObject): string {
+    if (uidObject.parentId != null) {
+        return uidObject.type + UID_SPLITTER + uidObject.primaryId + UID_SPLITTER + uidObject.parentId
+    }
+    return uidObject.type + UID_SPLITTER + uidObject.primaryId
+}
