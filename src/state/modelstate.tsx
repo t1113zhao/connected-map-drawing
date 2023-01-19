642915105b84deb13575e2a6ae879c537932137b
t1113zhao <zhaotian1998@gmail.com>
2023-01-18 20:20:02 -0800

    transferstate

--- src/state/modelstate.tsx
+++ src/state/modelstate.tsx
@@ -2,19 +2,6 @@ import {LatLng} from 'leaflet'
 import {StationPair} from '../state/stations/stationState'
 import {StateIdType} from './uids'
 
-export enum TransferType {
-    IN_STATION = "IN_STATION",
-    FREE_OUT_STATION = "FREE_OUT_STATION",
-    PAID_OUT_STATION = "PAID_OUT_STATION"
-}
-
-export type Transfer = {
-    uid: string,
-    number: string,
-    stationPair: StationPair,
-    type: TransferType
-}
-
 export type LatLngPair = [LatLng, LatLng]
 
 export type Agency = {
---
+++ src/state/transfers/transferState.tsx
@@ -0,0 +1,31 @@
+import { StationPair } from "../stations/stationState"
+import { LatLngPair } from "../modelstate"
+
+export enum TransferType {
+    IN_STATION = "IN_STATION",
+    FREE_OUT_STATION = "FREE_OUT_STATION",
+    PAID_OUT_STATION = "PAID_OUT_STATION"
+}
+
+export type Transfer = {
+    uid: string,
+    number: string,
+    stationPair: StationPair,
+    stationLatLngs: LatLngPair,
+    type: TransferType
+}
+
+export type AddTransferPayload = {
+    stationPair: StationPair,
+    stationLatLngs: LatLngPair,
+    type: TransferType
+}
+
+export type EditTransferPayload = {
+    uid: string,
+    type: TransferType
+}
+
+export type RemoveTransferPayload = {
+    uid: string
+}
