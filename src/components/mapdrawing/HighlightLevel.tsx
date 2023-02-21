e474e26ede20c8a56253a5b35c18b28989c69f73
t1113zhao <zhaotian1998@gmail.com>
2023-02-20 22:25:59 -0800

    Create HighlightLevel.tsx

---
+++ src/components/mapdrawing/HighlightLevel.tsx
@@ -0,0 +1,8 @@
+import { StateIdType } from "../../state/uids"
+
+export enum HighlightLevel {
+    SELECTED = "SELECTED",
+    ADJACENT = "ADJACENT",
+    NORMAL = "NORMAL",
+    UNSELECTED = "UNSELECTED",
+}
