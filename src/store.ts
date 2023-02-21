31b8bb22ed2bd3e4d1a3f4488120f943908bf983
t1113zhao <zhaotian1998@gmail.com>
2023-02-20 22:25:01 -0800

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,11 @@
 import { configureStore, Store } from '@reduxjs/toolkit'
 
-const store = configureStore({
-    reducer: {
-        viewState: viewReducer,
-        modelState: modelReducer
-    }
-})
+// const store = configureStore({
+//     reducer: {
+//         viewState: viewReducer,
+//         modelState: modelReducer
+//     }
+// })
 
-export type RootState = ReturnType<typeOf store.getState>
-export type AppDispatch = typeof store.dispatch
+// export type RootState = ReturnType<typeOf store.getState>
+// export type AppDispatch = typeof store.dispatch
